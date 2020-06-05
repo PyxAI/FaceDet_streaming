f3ed6dd59dc5e9f797f48b4799056d00583ec807
Omri Algazi <omrialgazi@gmail.com>
2020-06-06 00:36:04 +0300

    faster face detection

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import utils
 from utils import get_face_crop, clear_buffer
 import torch
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -11,72 +12,100 @@ import pickle
 from configparser import ConfigParser
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = "Detect faces trained from dataset")
-parser.add_argument("-input", choices = ["webcam", "ipcam"], help = "Choose either connected webcam or external ipcam", required = True)
-args = parser.parse_args()
 
+# From settings file
 settings = ConfigParser()
 settings.read("settings.ini")
 class_model_name = settings.get('Training','class_model_name').lower()
-frame_rate = settings.get('Detection','frame_rate').lower()
+try:
+	frame_rate =int(settings.get('Detection','frame_rate'))
+except ValueError:
+	print ("Frame_rate in settings file cannot be converted to int")
+
+
+# From command line args
+parser = ArgumentParser(description = "Detect faces trained from dataset")
+parser.add_argument("-input", choices = ["webcam", "ipcam"], \
+	help = "Choose either connected webcam or external ipcam", required = True)
+parser.add_argument("-user", help = "enter username for ipcam ", required = False)
+parser.add_argument("-password", help = "enter username for ipcam", required = False)
+args = parser.parse_args()
+# Set input source
+if args.input =="ipcam":
+	in_src = settings.get('Detection','ipcam')
+	if args.user and args.password:
+		import re
+		user = args.user
+		password = args.password
+		user_pos = re.search('//',in_src).start() + 2
+		in_src = in_src[:user_pos]+ user+':'+password +'@'+in_src[user_pos:] #construct user pass
+		print (in_src)
+elif args.input == "webcam":
+	in_src = 0
+elif args.input == "video":
+	print ("Not implemented yet")
+
+
 font  = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
 fontColor = (255,0,0)
 lineType = 2
-
 # TODO
-
-# improve speed by only using the mtcnn once for bboxes and crops
 
 # imrpove speed by using fast mtcnn
 
-
 #Get from webcam or ipcam
 clf = load('trained_models/' + class_model_name + '.joblib') 
-mtcnn = face_extractor(device)
+mtcnn = face_extractor(device, scale_factor = 0.5, margin = 10)
 resnet = get_resnet(device, classify = True)
 norm = get_norm('l2')
 convert_labels = pickle.load(open('trained_models/label_enc.pkl','rb'))
 
-if args.input == "webcam":
-	in_src = 0
-else:
-	in_src = settings.get('Detection','ipcam')
 cap = cv2.VideoCapture(in_src)
+
 while(True):
 	# Get frame
 	ret, frame = cap.read()
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	
+	try:
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	except cv2.error as err:
+		print ("Check input stream, user-name and password")
+		exit(2)
 	# Run through mtcnn to get a cropped face
-	crop = get_face_crop(frame, device)
-	if crop is not None:
-		
-		# Get draw bboxes on and draw them
-		bbox, probs = mtcnn.detect(frame)
+	bbox_list, probs = mtcnn.detect(frame)
+	if bbox_list is not None:
+		for bbox in bbox_list:
+			bbox = [int(x) for x in bbox]
+			x1, y1, x2, y2 = (bbox[i] for i in range(4))
+			crop = frame[x1:x2,y1:y2,:]
+			crop = np.moveaxis(crop, -1, 0)
+			crop = torch.Tensor(crop)
 
-		# To verify we got a bbox, (comes as an array, or None) we run throught two "if"s
-		if isinstance(probs,np.ndarray):
-			if probs.any():
-				start_point = (bbox[0][0],bbox[0][1])
-				end_point = (bbox[0][2],bbox[0][3])
-				frame = cv2.rectangle(frame, start_point, end_point, (255,0,0), 2) 
+			# Get draw bboxes on and draw them
+			
+			# To verify we got a bbox, (comes as an array, or None) we run throught two "if"s
+			if isinstance(probs,np.ndarray):
+				if probs.any():
+					start_point = (x1, y1)
+					end_point = (x2, y2)
+					print (start_point, end_point)
+					frame = cv2.rectangle(frame, start_point, end_point, (255,0,0), 2) 
 
-		# Get faceID only if we have a crop
-		embeddings = resnet(crop.unsqueeze(0)).detach().numpy()
-		embeddings = (embeddings- embeddings.mean()) / embeddings.std()
-		# Predict!
-		prob = clf.predict_proba(embeddings)
-		pred = np.argmax(prob)
-		face = convert_labels.inverse_transform([pred])[0] + "\nprobability: {:.4f}".format(prob[0][pred])
-		print("pred = {}, prob = {}".format(pred,prob))
-		# Draw text
-		bottomLeftCornerOfText = (int(start_point[0]),int(start_point[1])-10)
-		frame = cv2.putText(frame, face+': ', bottomLeftCornerOfText, font, fontScale, color=fontColor, lineType=lineType)
-		
-		# For debugging
-		#display(Image.open(base+'who_crop.jpg'))
-		print(face)
+			# Get faceID only if we have a crop
+			embeddings = resnet(crop.unsqueeze(0)).detach().numpy()
+			embeddings = (embeddings- embeddings.mean()) / embeddings.std()
+			# Predict!
+			prob = clf.predict_proba(embeddings)
+			pred = np.argmax(prob)
+			face = convert_labels.inverse_transform([pred])[0] + "\nprobability: {:.4f}".format(prob[0][pred])
+			print("pred = {}, prob = {}".format(pred,prob))
+			# Draw text
+			bottomLeftCornerOfText = (int(start_point[0]),int(start_point[1])-10)
+			frame = cv2.putText(frame, face+': ', bottomLeftCornerOfText, font, fontScale, color=fontColor, lineType=lineType)
+			
+			# For debugging
+			#display(Image.open(base+'who_crop.jpg'))
+			print(face)
 
 	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 	cv2.imshow('frame',frame)
